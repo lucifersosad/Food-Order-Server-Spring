ac0e4d53bd135611e5c4226cf0b06eeba8270ea9
Lucifer <luciferxhnh@gmail.com>
2024-05-19 22:09:16 +0700

    update ràng buộc

--- src/main/java/spring/api/uteating/model/ProductDTO.java
+++ src/main/java/spring/api/uteating/model/ProductDTO.java
@@ -1,5 +1,6 @@
 package spring.api.uteating.model;
 
+import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
@@ -21,9 +22,10 @@ public class ProductDTO {
     @NotNull(message = "Kieu không được để trống")
     private String productType;
     @NotNull(message="Giá không được để trống")
+    @Max(value = 10000000, message = "Nhieu nhat 10 trieu")
     private Integer productPrice;
     @NotNull(message = "Số lượng không được để trống")
-    @Min(value = 100, message = "It nhat 100 san pham")
+    @Min(value = 1, message = "It nhat 1 san pham")
     private Integer remainAmount;
     @NotNull(message = "Mô tả không được để trống")
     private String description;
